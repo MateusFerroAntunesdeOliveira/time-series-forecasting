f9fcc5d63d8027eadecab31237107a25f36a67bf
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-05 19:14:36 -0300

    fix: change the code to english

--- src/main.py
+++ src/main.py
@@ -1,56 +1,54 @@
 import pandas as pd
 import os
 
-input_folder = './readings/'
-output_folder = './processed/'
+input_folder = "./readings/"
+output_folder = "./processed/"
 
 os.makedirs(output_folder, exist_ok=True)
 
-# Lista de arquivos a serem processados
-# filenames = [f'FATOR_CAPACIDADE-2_{year}_{month:02d}.csv' for year in range(2022, 2024) for month in range(1, 13)]
-filenames = ["FATOR_CAPACIDADE-2_2024_02.csv"]
+# File List to be processed
+filenames = [f"FATOR_CAPACIDADE-2_{year}_{month:02d}.csv" for year in range(2022, 2024) for month in range(1, 13)]
+# filenames = ["FATOR_CAPACIDADE-2_2024_02.csv"]
 
-# Colunas a serem removidas
-columns_to_remove = ['id_subsistema', 'nom_subsistema', 'id_estado', 'nom_localizacao', 'val_latitudesecoletora', 
-                     'val_longitudesecoletora', 'val_latitudepontoconexao', 'val_longitudepontoconexao', 'id_ons', 'ceg']
+# Columns to be removed
+columns_to_remove = ["id_subsistema", "nom_subsistema", "id_estado", "nom_localizacao", "val_latitudesecoletora", 
+                     "val_longitudesecoletora", "val_latitudepontoconexao", "val_longitudepontoconexao", "id_ons", "ceg"]
 
-# Nome do complexo a ser filtrado
-complex_name = 'Conj. Alex'
-
-# Nome do estado a ser filtrado
-state_name = 'CE'
+# Complex name and State Name to be filtered
+complex_name = "Conj. Alex"
+state_name = "CE"
 
 for filename in filenames:
-    # Caminho completo do arquivo de entrada
+    # Full path of the input file
     input_path = os.path.join(input_folder, filename)
-    processed_path = os.path.join(output_folder, f'processed_{filename}')
+    processed_path = os.path.join(output_folder, f"processed_{filename}")
     
     if os.path.exists(processed_path):
-        print(f'{filename} já foi processado. Pulando para o próximo arquivo.')
+        print(f"{filename} already processed. Skipping to the next file...")
         continue
 
     if os.path.exists(input_path):
-        # Ler o arquivo CSV
-        df = pd.read_csv(input_path, sep=';')
+        # Read the CSV file
+        df = pd.read_csv(input_path, sep=";")
         
-        # Filtrar pelo estado CE
-        df_ce = df[df['id_estado'] == state_name]
+        # Filter by state name
+        df_ce = df[df["id_estado"] == state_name]
         
-        # Remover colunas desnecessárias
+        # Remove unnecessary columns
         df_ce = df_ce.drop(columns=columns_to_remove)
         
-        # Filtrar pelo nome do complexo
-        df_alex = df_ce[df_ce['nom_usina_conjunto'] == complex_name]
+        # Filter by complex name
+        df_alex = df_ce[df_ce["nom_usina_conjunto"] == complex_name]
         
-        # Caminho completo do arquivo de saída
-        output_filename = f'processed_{filename}'
+        # Full path of the output file
+        output_filename = f"processed_{filename}"
         output_path = os.path.join(output_folder, output_filename)
         
-        # Salvar o resultado em um novo arquivo CSV
-        df_alex.to_csv(output_path, sep=';', index=False)
+        # Save the processed file
+        df_alex.to_csv(output_path, sep=";", index=False)
         
-        print(f'{output_filename} salvo com sucesso.')
+        print(f"{output_filename} saved.")
     else:
-        print(f'Arquivo {filename} não encontrado.')
+        print(f"File {filename} not found.")
 
-print('Processamento concluído.')
+print("\nProcessing finished.\n")
