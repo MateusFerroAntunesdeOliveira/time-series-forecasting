7487fbd1fb8be8c8a351e213de979655c580f1fe
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-05 19:17:37 -0300

    feat: adding 2024 readings

--- src/main.py
+++ src/main.py
@@ -6,9 +6,11 @@ output_folder = "./processed/"
 
 os.makedirs(output_folder, exist_ok=True)
 
-# File List to be processed
+# File List to be processed - Will be used to train the script
 filenames = [f"FATOR_CAPACIDADE-2_{year}_{month:02d}.csv" for year in range(2022, 2024) for month in range(1, 13)]
-# filenames = ["FATOR_CAPACIDADE-2_2024_02.csv"]
+
+# Adding the first 5 months of 2024 - Will be used to test the script
+filenames.extend([f"FATOR_CAPACIDADE-2_2024_{month:02d}.csv" for month in range(1, 6)])
 
 # Columns to be removed
 columns_to_remove = ["id_subsistema", "nom_subsistema", "id_estado", "nom_localizacao", "val_latitudesecoletora", 
