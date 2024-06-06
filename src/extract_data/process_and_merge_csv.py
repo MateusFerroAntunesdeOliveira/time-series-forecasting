7bd7ef7cbcdbd798573d03748a71660c62be9dae
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-05 22:26:01 -0300

    feat: add logs to the application

--- src/extract_data/process_and_merge_csv.py
+++ src/extract_data/process_and_merge_csv.py
@@ -1,6 +1,9 @@
 import sys
 import os
 
+import logging
+import colorlog
+
 import pandas as pd
 
 # Define file paths
@@ -8,7 +11,34 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 extract_dir = os.path.join(current_dir)
 sys.path.append(extract_dir)
 
-from extract_data.process_and_merge_csv_config import INPUT_CSV_FILE_PATH, OUTPUT_CSV_FILE_PATH, OUTPUT_MERGED_CSV_FILE_PATH, OUTPUT_MERGED_CSV_FILE_NAME
+from extract_data.process_and_merge_csv_config import LOG_LEVEL, LOG_LEVEL_FORMAT, INPUT_PATH, OUTPUT_PATH, OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME
+
+# Create a color log formatter
+formatter = colorlog.ColoredFormatter(
+    LOG_LEVEL_FORMAT,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    log_colors={
+        'DEBUG': 'blue',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+
+# Configure the logger
+logging.basicConfig(level=LOG_LEVEL)
+logger = logging.getLogger()
+
+# Remove all handlers from the root logger
+root_logger = logging.getLogger()
+if root_logger.handlers:
+    root_logger.handlers.clear()
+
+# Create a new handler and set the colored formatter
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # File List to be processed - Will be used to train the script
 filenames = [f"FATOR_CAPACIDADE-2_{year}_{month:02d}.csv" for year in range(2022, 2024) for month in range(1, 13)]
@@ -34,62 +64,80 @@ def check_file_exists(filepath):
     return os.path.exists(filepath)
 
 def read_csv_file(filepath):
+    logger.debug(f"Reading file {filepath}")
     return pd.read_csv(filepath, sep=";", encoding="utf-8")
 
 def filter_by_state(df, state_name):
+    logger.debug(f"Filtering data by state {state_name}")
     return df[df["id_estado"] == state_name]
 
 def remove_columns(df, columns_to_remove):
+    logger.debug(f"Removing columns {columns_to_remove}")
     return df.drop(columns=columns_to_remove)
 
 def filter_by_complex_name(df, complex_name):
+    logger.debug(f"Filtering data by complex name {complex_name}")
     return df[df["nom_usina_conjunto"] == complex_name]
 
 def save_csv_file(df, filepath):
+    logger.debug(f"Saving file {filepath}")
     df.to_csv(filepath, sep=";", index=False)
 
 def load_processed_data():
     global df_all
-    merged_file_path = join_file_path(OUTPUT_MERGED_CSV_FILE_PATH, OUTPUT_MERGED_CSV_FILE_NAME)
+    merged_file_path = join_file_path(OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME)
     if check_file_exists(merged_file_path):
+        logger.info(f"Loading the merged file {merged_file_path}")
         df_all = read_csv_file(merged_file_path)
-        print("Merged file loaded.")
-        return
-
-    processed_files = [f"processed_{filename}" for filename in filenames]
-    for filename in processed_files:
-        processed_file_path = join_file_path(OUTPUT_CSV_FILE_PATH, filename)
-        if check_file_exists(processed_file_path):
-            df_processed = read_csv_file(processed_file_path)
-            df_all = pd.concat([df_all, df_processed], ignore_index=True)
+    else:
+        logger.info("Merged file not found. Loading individual processed files.")
+        processed_files = [f"processed_{filename}" for filename in filenames]
+        for filename in processed_files:
+            processed_file_path = join_file_path(OUTPUT_PATH, filename)
+            if check_file_exists(processed_file_path):
+                df_processed = read_csv_file(processed_file_path)
+                df_all = pd.concat([df_all, df_processed], ignore_index=True)
+    logger.info("Processed data loaded successfully.")
 
 def process_files():
     global df_all
+    logger.info("Starting file processing...")
+
     # Load the already processed data
     load_processed_data()
 
     for filename in filenames:
-        input_file_path = join_file_path(INPUT_CSV_FILE_PATH, filename)
-        processed_file_path = join_file_path(OUTPUT_CSV_FILE_PATH, f"processed_{filename}")
+        input_file_path = join_file_path(INPUT_PATH, filename)
+        processed_file_path = join_file_path(OUTPUT_PATH, f"processed_{filename}")
 
         if check_file_exists(processed_file_path):
-            print(f"{filename} already processed. Skipping to the next file...")
+            logger.info(f"File {filename} already processed. Skipping...")
             continue
 
         if check_file_exists(input_file_path):
-            df = read_csv_file(input_file_path)
-            df = filter_by_state(df, state_name)
-            df = remove_columns(df, columns_to_remove)
-            df = filter_by_complex_name(df, complex_name)
-            save_csv_file(df, processed_file_path)
-            print(f"{filename} processed and saved.")
+            try:
+                df = read_csv_file(input_file_path)
+                df = filter_by_state(df, state_name)
+                df = remove_columns(df, columns_to_remove)
+                df = filter_by_complex_name(df, complex_name)
+                save_csv_file(df, processed_file_path)
+                logger.info(f"File {filename} processed and saved.")
 
-            # Merge the processed data to the main dataframe
-            df_all = pd.concat([df_all, df], ignore_index=True)
+                # Merge the processed data to the main dataframe
+                df_all = pd.concat([df_all, df], ignore_index=True)
+            except Exception as e:
+                logger.error(f"Error processing file {filename}. Error: {e}")
         else:
-            print(f"File {filename} not found.")
+            logger.error(f"File {filename} not found.")
 
     # Save the merged dataframe to a CSV file
-    save_csv_file(df_all, join_file_path(OUTPUT_MERGED_CSV_FILE_PATH, OUTPUT_MERGED_CSV_FILE_NAME))
+    save_csv_file(df_all, join_file_path(OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME))
+    logger.info("Merged file saved successfully.")
+    logger.info("Processing finished.")
 
-    print("\nProcessing finished.\n")
+
+# TODO:
+# Fazer com que a Coluna "din_instante" (que é a data e hora da leitura) seja como se fosse um identificador primário, onde não pode haver duplicatas
+
+# TODO:
+# Mover a parte de Log para um arquivo de configuração de log específico
