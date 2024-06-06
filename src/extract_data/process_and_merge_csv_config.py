0fa3267ba363245d38ff1c5bf25c9f2e57c7c373
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-05 22:25:38 -0300

    feat: add log_level and log_level_format to config

--- src/extract_data/process_and_merge_csv_config.py
+++ src/extract_data/process_and_merge_csv_config.py
@@ -5,8 +5,12 @@ from dotenv import load_dotenv
 # Load the environment variables
 load_dotenv()
 
-# Access the environment variables
-INPUT_CSV_FILE_PATH = os.getenv("INPUT_CSV_FILE_PATH")
-OUTPUT_CSV_FILE_PATH = os.getenv("OUTPUT_CSV_FILE_PATH")
-OUTPUT_MERGED_CSV_FILE_PATH = os.getenv("OUTPUT_MERGED_CSV_FILE_PATH")
-OUTPUT_MERGED_CSV_FILE_NAME = os.getenv("OUTPUT_MERGED_CSV_FILE_NAME")
+# Access the log configuration environment variables
+LOG_LEVEL = os.getenv("LOG_LEVEL")
+LOG_LEVEL_FORMAT = os.getenv("LOG_LEVEL_FORMAT")
+
+# Access the file environment variables
+INPUT_PATH = os.getenv("INPUT_PATH")
+OUTPUT_PATH = os.getenv("OUTPUT_PATH")
+OUTPUT_MERGED_PATH = os.getenv("OUTPUT_MERGED_PATH")
+OUTPUT_MERGED_FILENAME = os.getenv("OUTPUT_MERGED_FILENAME")
