3a109d3ee823abd7d69a8a6b332817bd857ef020
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-10 20:57:36 -0300

    feat: add basic statistics information

--- src/data_processing/data_science_fundamentals.py
+++ src/data_processing/data_science_fundamentals.py
@@ -1,23 +1,56 @@
 import sys
 import os
-
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from data_processing.config import logger, OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME
+from data_processing.load_constants import target_column
+import utility.utils as utils
 
 # Define file paths
 current_dir = os.path.dirname(os.path.abspath(__file__))
 extract_dir = os.path.join(current_dir)
 sys.path.append(extract_dir)
 
-from data_processing.config import logger, OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME
-import utility.utils as utils
-
 def read_csv_file_as_dataframe(file_path):
     logger.debug(f"Reading file {file_path}")
-    return pd.read_csv(file_path)
+    return pd.read_csv(file_path, sep=';', encoding='utf-8')
 
 def check_missing_values(df):
     logger.debug("Checking for missing values")
     return df.isnull().sum()
+
+def filter_zero_values(df, column_name):
+    logger.info("Filtering zero values")
+    return df[df[column_name] != 0]
+
+def apply_statistics(df):
+    logger.info("Applying statistics")
+    return df.describe()
+   
+def plot_symmetry(df, column_name):
+    logger.info("Plotting symmetry")
+    mean, median, mode = evaluate_mean_median_mode(df, column_name)
+    plot_graph(df, mean, median, mode, column_name)
+    logger.info("Symmetry plot created")
+
+def evaluate_mean_median_mode(df, column_name):
+    logger.info("Evaluating mean, median, and mode")
+    return df[column_name].mean(), df[column_name].median(), df[column_name].mode()[0]
+
+def plot_graph(df, mean, median, mode, column_name):
+    plt.figure(figsize=(10, 6))
+    # Kernel Density Estimation (KDE) is a way to estimate the probability density function of a continuous random variable.
+    sns.histplot(df[column_name], bins=30, kde=True, color='blue', stat='density')
+    plt.axvline(mean, color='red', linestyle='--', linewidth=2, label=f'Mean: {mean:.2f}')
+    plt.axvline(median, color='green', linestyle='-', linewidth=2, label=f'Median: {median:.2f}')
+    plt.axvline(mode, color='purple', linestyle='-', linewidth=2, label=f'Mode: {mode:.2f}')
+    plt.title(f'Distribution of {column_name} with Measures of Central Tendency')
+    plt.xlabel(column_name)
+    plt.ylabel('Density')
+    plt.legend()
+    plt.show()
 
 def apply_measures():
     logger.info("Applying measures")
@@ -25,8 +58,17 @@ def apply_measures():
     merged_csv_file = utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME)
     df_all = read_csv_file_as_dataframe(merged_csv_file)
 
+    df_filtered = filter_zero_values(df_all, target_column)
+
+    # Describe the dataset
+    summary_stats = apply_statistics(df_filtered)
+    logger.info(f"Summary Statistics:\n{summary_stats}")
+    
     # Check for missing values
     missing_values = check_missing_values(df_all)
     logger.info(f"Missing values:\n{missing_values}")
+
+    # Plot symmetry
+    plot_symmetry(df_filtered, target_column)
 
     logger.info("Measures applied")
