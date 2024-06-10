39f0d25afdffc5b297586ddfc23e3014a057c607
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-10 18:43:07 -0300

    fix: update load constants

--- src/data_processing/load_constants.py
+++ src/data_processing/load_constants.py
@@ -1,10 +1,12 @@
-
-# File List to be processed - Will be used to train the script & adding the first 5 months of 2024 - Will be used to test the script
+# File List to be processed - Will be used to train
 filenames = [f"FATOR_CAPACIDADE-2_{year}_{month:02d}.csv" for year in range(2022, 2024) for month in range(1, 13)]
+
+# Adding the first 5 months of 2024 - Will be used to test
 filenames.extend([f"FATOR_CAPACIDADE-2_2024_{month:02d}.csv" for month in range(1, 6)])
 
 # Columns to be removed
-columns_to_remove = ["id_subsistema", "nom_subsistema", "id_estado", "nom_localizacao", "val_latitudesecoletora", "val_longitudesecoletora", "val_latitudepontoconexao", "val_longitudepontoconexao", "id_ons", "ceg"]
+columns_to_remove = ["id_subsistema", "nom_subsistema", "id_estado", "nom_localizacao", "val_latitudesecoletora", 
+                    "val_longitudesecoletora", "val_latitudepontoconexao", "val_longitudepontoconexao", "id_ons", "ceg"]
 
 # Complex name and State Name to be filtered
 complex_name = "Conj. Alex"
