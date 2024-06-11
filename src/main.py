141f25d85608d723dc15118f0ba8f8b9a0a978f3
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-10 21:52:14 -0300

    feat: add data cleaning to project

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import data_processing.process_and_merge_csv as process_and_merge_csv
 import data_processing.data_visualization as data_visualization
+import data_processing.data_cleaning as data_cleaning
 
 def main():
-    # process_and_merge_csv.process_files()
+    process_and_merge_csv.process_files()
     data_visualization.apply_measures()
+    data_cleaning.clean_data()
 
 if __name__ == "__main__":
     main()
