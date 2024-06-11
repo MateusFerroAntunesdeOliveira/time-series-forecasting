90add5e2c62b72343cc7580c82351c2a2db6c943
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-11 19:07:55 -0300

    feat: add flow to generate full and filtered merged file

--- src/data_processing/process_and_merge_csv.py
+++ src/data_processing/process_and_merge_csv.py
@@ -8,12 +8,14 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 extract_dir = os.path.join(current_dir)
 sys.path.append(extract_dir)
 
-from data_processing.config import logger, INPUT_PATH, OUTPUT_PATH, OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME
+from data_processing.config import logger, INPUT_PATH, OUTPUT_PATH, OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME, OUTPUT_FILTERED_MERGED_FILENAME
 from data_processing.load_constants import filenames, columns_to_remove, complex_name, state_name, reading_date_column
 import utility.utils as utils
 
-# Dataframe to store all the processed data
+# df_all to store the merged dataframes with all columns
 df_all = pd.DataFrame()
+# df_all_filtered to store the merged dataframes with filtered columns
+df_all_filtered = pd.DataFrame()
 
 def read_csv_file(filepath):
     logger.debug(f"Reading file {filepath}")
@@ -44,11 +46,18 @@ def merge_files(df_all, df):
     return pd.concat([df_all, df], ignore_index=True)
 
 def load_processed_data():
-    global df_all
+    global df_all, df_all_filtered
     merged_file_path = utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME)
+    filtered_merged_file_path = utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_FILTERED_MERGED_FILENAME)
+    
     if utils.check_file_exists(merged_file_path):
         logger.info(f"Loading the merged file {merged_file_path}")
         df_all = read_csv_file(merged_file_path)
+
+    if utils.check_file_exists(filtered_merged_file_path):
+        logger.info(f"Loading the filtered merged file {filtered_merged_file_path}")
+        df_all_filtered = read_csv_file(filtered_merged_file_path)
+
     else:
         logger.info("Merged file not found. Loading individual processed files.")
         processed_files = [f"processed_{filename}" for filename in filenames]
@@ -56,11 +65,13 @@ def load_processed_data():
             processed_file_path = utils.join_file_path(OUTPUT_PATH, filename)
             if utils.check_file_exists(processed_file_path):
                 df_processed = read_csv_file(processed_file_path)
-                df_all = pd.concat([df_all, df_processed], ignore_index=True)
+                df_all = merge_files(df_all, df_processed)
+                df_all_filtered = merge_files(df_all_filtered, df_processed)
+
     logger.info("Processed data loaded successfully.")
 
 def process_files():
-    global df_all
+    global df_all, df_all_filtered
     logger.info("Starting file processing...")
 
     # Create the output directories if they don't exist
@@ -73,6 +84,7 @@ def process_files():
     for filename in filenames:
         input_file_path = utils.join_file_path(INPUT_PATH, filename)
         processed_file_path = utils.join_file_path(OUTPUT_PATH, f"processed_{filename}")
+        processed_filtered_file_path = utils.join_file_path(OUTPUT_PATH, f"processed_filtered_{filename}")
 
         if utils.check_file_exists(processed_file_path):
             logger.info(f"File {filename} already processed. Skipping...")
@@ -84,12 +96,16 @@ def process_files():
                 df = filter_by_state(df, state_name)
                 df = filter_by_complex_name(df, complex_name)
                 df = remove_duplicates(df, reading_date_column)
-                df = remove_columns(df, columns_to_remove)
                 save_csv_file(df, processed_file_path)
+
+                df_filtered = remove_columns(df, columns_to_remove)
+                save_csv_file(df, processed_filtered_file_path)
+
                 logger.info(f"File {filename} processed and saved.")
 
                 # Merge the processed data to the main dataframe
                 df_all = merge_files(df_all, df)
+                df_all_filtered = merge_files(df_all_filtered, df_filtered)
 
             except Exception as e:
                 logger.error(f"Error processing file {filename}. Error: {e}")
@@ -98,5 +114,6 @@ def process_files():
 
     # Save the merged dataframe to a CSV file
     save_csv_file(df_all, utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME))
+    save_csv_file(df_all_filtered, utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_FILTERED_MERGED_FILENAME))
     logger.info("Merged file saved successfully.")
     logger.info("Processing finished.")
