e82a734d2b442c9bb8a1a1537a6525acda8e05e1
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-11 18:24:33 -0300

    feat: add correlation analysis

--- src/data_processing/data_visualization.py
+++ src/data_processing/data_visualization.py
@@ -48,24 +48,46 @@ def plot_symmetry(df, column_name):
 def evaluate_mean_median_mode(df, column_name):
     logger.info("Evaluating mean, median, and mode")
     return df[column_name].mean(), df[column_name].median(), df[column_name].mode()[0]
-    
+
+def remove_constant_columns(df):
+    logger.info("Removing constant columns")
+    return df.loc[:, df.apply(pd.Series.nunique) != 1]
+
+def get_correlation(df):
+    logger.info("Calculating correlation")
+    return df.corr()
+
+def plot_correlation_matrix(corr):
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f', linewidths=.5)
+    plt.title('Correlation Heatmap')
+    plt.show()
+
+def get_correlation_heatmap(df):
+    logger.info("Plotting correlation heatmap")
+    df = remove_constant_columns(df)
+    correlation = get_correlation(df)
+    plot_correlation_matrix(correlation)
+    logger.info("Correlation heatmap created")
+
 def apply_measures():
     logger.info("Applying measures")
-
     merged_csv_file = utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME)
-    df_all = read_csv_file_as_dataframe(merged_csv_file)
 
     # Describe the dataset & Plot symmetry with original dataset
+    df_all = read_csv_file_as_dataframe(merged_csv_file)
     summary_stats = apply_statistics(df_all)
     logger.info(f"Summary Statistics with original dataset:\n{summary_stats}")
     plot_symmetry(df_all, target_column)
 
     # Filter zero values before applying statistics - In this case, the zero values are not good for the analysis
-    df_filtered = filter_zero_values(df_all, target_column)
-
     # Describe the dataset & Plot symmetry with filtered dataset
+    df_filtered = filter_zero_values(df_all, target_column)
     summary_stats = apply_statistics(df_filtered)
     logger.info(f"Summary Statistics with filtered dataset:\n{summary_stats}")
     plot_symmetry(df_filtered, target_column)
+
+    # Plot correlation heatmap with filtered dataset
+    get_correlation_heatmap(df_filtered)
 
     logger.info("Measures applied")
