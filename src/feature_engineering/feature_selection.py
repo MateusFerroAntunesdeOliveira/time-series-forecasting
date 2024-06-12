7d9771e3719ab640f10d3cc895bf75f4887349a8
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-11 22:26:20 -0300

    feat: add feature_selection based on variance

--- src/feature_engineering/feature_selection.py
+++ src/feature_engineering/feature_selection.py
@@ -2,6 +2,9 @@ import sys
 import os
 import pandas as pd
 
+# Sklearn Imports
+from sklearn.feature_selection import VarianceThreshold
+
 # Custom Imports
 from utility.config import logger, OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME
 import utility.utils as utils
@@ -12,23 +15,14 @@ extract_dir = os.path.join(current_dir)
 sys.path.append(extract_dir)
 
 def feature_selection_based_on_correlation(df, correlation_threshold):
-    """
-    Perform feature selection based on Pearson and Spearman correlation.
-    
-    :param df: DataFrame containing features and target.
-    :param correlation_threshold: Threshold for selecting features based on correlation.
-    :return: DataFrame with selected features.
-    """
+    """ Perform feature selection based on Pearson and Spearman correlation. """
     # Separate features and target - assuming the target is the last column
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1]
 
-    # Calculate Pearson correlation - linear relationship
+    # Calculate Pearson & Spearman correlation - linear relationship & monotonic relationship
     pearson_corr = X.corrwith(y, method="pearson").abs()
-
-    # Calculate Spearman correlation - monotonic relationship
     spearman_corr = X.corrwith(y, method="spearman").abs()
-
     # Combine results
     correlation_scores = pd.DataFrame({"Pearson": pearson_corr, "Spearman": spearman_corr})
 
@@ -36,23 +30,49 @@ def feature_selection_based_on_correlation(df, correlation_threshold):
     pearson_selected_features = correlation_scores[correlation_scores["Pearson"] > correlation_threshold].index.tolist()
     spearman_selected_features = correlation_scores[correlation_scores["Spearman"] > correlation_threshold].index.tolist()
 
-    logger.info("Features selected using Pearson correlation: %s", pearson_selected_features)
-    logger.info("Features selected using Spearman correlation: %s", spearman_selected_features)
-
-    # Create DataFrame with selected features
     selected_features = list(set(pearson_selected_features + spearman_selected_features))
-    selected_df = df[selected_features + [df.columns[-1]]]  # Add target column to the selected features DataFrame
+    selected_dataframe = create_dataframe_with_selected_features(df, selected_features)
+    return selected_features, selected_dataframe
 
-    return selected_df
+def feature_selection_based_on_variance(df, variance_threshold):
+    """ Perform feature selection based on variance threshold. """
+    # Select only numeric columns
+    numeric_df = df.select_dtypes(include=["number"])
+
+    # Separate features and target - assuming the target is the last column
+    X = numeric_df.iloc[:, :-1]
+
+    # Apply VarianceThreshold
+    selector = VarianceThreshold(threshold=variance_threshold)
+    selector.fit(X)
+
+    # Get support and selected feature names
+    selected_feature_indices = selector.get_support(indices=True)
+    selected_features = X.columns[selected_feature_indices].tolist()
+    selected_dataframe = create_dataframe_with_selected_features(df, selected_features)
+    return selected_features, selected_dataframe
+
+def create_dataframe_with_selected_features(df, selected_features):
+    """ Create a new DataFrame with the selected features and the target column. """
+    return df[selected_features + [df.columns[-1]]]
 
 def apply_feature_selection():
     logger.info("Applying feature selection")
-    
+
     # Read the filtered merged file
     df = utils.read_csv_file_as_dataframe(utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_MERGED_FILENAME))
     logger.info(f"Original DataFrame shape: {df.shape}")
-    
+
     # Perform feature selection based on correlation
-    selected_df = feature_selection_based_on_correlation(df, correlation_threshold=0.5)
-    logger.info(f"Selected DataFrame shape: {selected_df.shape}")
-    logger.info(f"Selected features based on correlation: {selected_df.columns.tolist()}")
+    selected_features_correlation, corr_df = feature_selection_based_on_correlation(df, correlation_threshold=0.5)
+    logger.info("")
+    logger.info(f"Selected features based on correlation: {selected_features_correlation}")
+    logger.info(f"DataFrame shape based on correlation: {corr_df.shape}")
+    logger.info(f"DataFrame Features: {corr_df.columns.to_list()}")
+
+    # Perform feature selection based on variance
+    selected_features_variance, var_df = feature_selection_based_on_variance(df, variance_threshold=0.1)
+    logger.info("")
+    logger.info(f"Selected features based on variance: {selected_features_variance}")
+    logger.info(f"DataFrame shape based on variance: {var_df.shape}")
+    logger.info(f"DataFrame Features: {var_df.columns.to_list()}")
