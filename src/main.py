a3325be7629d115abff921aba090d9b6b12c26ed
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-12 18:54:11 -0300

    fix: reorder the import and the flow

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-import data_processing.process_and_merge_csv as process_and_merge_csv
-import data_processing.data_visualization as data_visualization
+import data_processing.process_data as process_data
 import data_processing.data_cleaning as data_cleaning
-import feature_engineering.predictive_features as predictive_features
+import data_processing.data_visualization as data_visualization
+import feature_engineering.feature_prediction as feature_prediction
 import feature_engineering.feature_selection as feature_selection
 
 def main():
-    # process_and_merge_csv.process_files()
-    # data_visualization.apply_measures()
-    # data_cleaning.clean_data()
-    # predictive_features.apply_classification()
+    process_data.process_files()
+    data_cleaning.clean_data()
+    data_visualization.apply_measures()
+    feature_prediction.apply_feature_prediction_classification()
     feature_selection.apply_feature_selection()
 
 if __name__ == "__main__":
