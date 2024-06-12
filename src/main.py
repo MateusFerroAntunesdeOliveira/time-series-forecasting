471f6a1eae49312ba0dde674ef5309a820d06ebc
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-11 21:31:41 -0300

    feat: add predictive_features to main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import data_processing.process_and_merge_csv as process_and_merge_csv
 import data_processing.data_visualization as data_visualization
 import data_processing.data_cleaning as data_cleaning
+import feature_engineering.predictive_features as predictive_features
 
 def main():
     process_and_merge_csv.process_files()
     data_visualization.apply_measures()
     data_cleaning.clean_data()
+    predictive_features.apply_classification()
 
 if __name__ == "__main__":
     main()
