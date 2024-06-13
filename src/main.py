fef5e3c351106a37a82a657024771918db75c6fd
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-12 21:50:36 -0300

    feat: add EDA to main

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ import feature_engineering.feature_selection as feature_selection
 import exploratory_data_analysis.data_exploration as data_exploration
 
 def main():
-    # process_data.process_files()
-    # data_cleaning.clean_data()
-    # data_visualization.apply_measures()
-    # feature_prediction.apply_feature_prediction_classification()
-    # feature_selection.apply_feature_selection()
+    process_data.process_files()
+    data_cleaning.clean_data()
+    data_visualization.apply_measures()
+    feature_prediction.apply_feature_prediction_classification()
+    feature_selection.apply_feature_selection()
     data_exploration.explore_data()
 
 if __name__ == "__main__":
