4e653062e1c329061f05406ba5e5177f565bd22c
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-15 11:24:48 -0300

    refactor: move common methods to utils

--- src/forecasting/lstm_time_series.py
+++ src/forecasting/lstm_time_series.py
@@ -10,45 +10,10 @@ from utility.config import logger, OUTPUT_MERGED_PATH, OUTPUT_FILTERED_MERGED_FI
 import utility.load_constants as constants
 import utility.utils as utils
 
-def load_data(file_path, parse_date, index_col):
-    return pd.read_csv(file_path, sep=";", parse_dates=[parse_date], index_col=index_col)
-
-def split_dataset(dataset, train_start_idx, train_end_idx, test_start_idx, test_end_idx):
-    train_data = dataset[train_start_idx:train_end_idx]
-    test_data = dataset[test_start_idx:test_end_idx]
-    return train_data, test_data
-
-def create_sequences(data, seq_length):
-    X, y = [], []
-    for i in range(len(data) - seq_length):
-        X.append(data[i:i + seq_length])
-        y.append(data[i + seq_length])
-    return np.array(X), np.array(y)
-
-def convert_string_to_datetime_and_add_index_position(df, train_start, train_end, test_start, test_end):
-    train_start = pd.to_datetime(train_start)
-    train_end = pd.to_datetime(train_end)
-    test_start = pd.to_datetime(test_start)
-    test_end = pd.to_datetime(test_end)
-
-    train_start_idx = df.index.get_loc(train_start, method='nearest')
-    train_end_idx = df.index.get_loc(train_end, method='nearest') + 1
-    test_start_idx = df.index.get_loc(test_start, method='nearest')
-    test_end_idx = df.index.get_loc(test_end, method='nearest') + 1
-
-    return train_start_idx, train_end_idx, test_start_idx, test_end_idx
-
-def get_future_text(future_hours):
-    hour_mapping = {
-        1: "1 hora",
-        12: "12 horas",
-        24: "1 dia",
-        48: "2 dias"
-    }
-    return hour_mapping.get(future_hours, f"{future_hours} horas")
-
 def apply_lstm_forecasting(future_hours):
-    df = load_data(utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_FILTERED_MERGED_FILENAME), constants.reading_date_column, constants.reading_date_column)
+    file_path = utils.join_file_path(OUTPUT_MERGED_PATH, OUTPUT_FILTERED_MERGED_FILENAME)
+    
+    df = utils.read_csv_file_as_dataframe_with_date_index(file_path, constants.reading_date_column, constants.reading_date_column)
     series = df[constants.target_column]
 
     # Normalize the dataset
@@ -56,12 +21,12 @@ def apply_lstm_forecasting(future_hours):
     scaled_data = scaler.fit_transform(series.values.reshape(-1, 1))
 
     # Split the dataset into training and test datasets
-    train_start_idx, train_end_idx, test_start_idx, test_end_idx = convert_string_to_datetime_and_add_index_position(df, constants.train_start, constants.train_end, constants.test_start, constants.test_end)
+    train_start_idx, train_end_idx, test_start_idx, test_end_idx = utils.convert_string_to_datetime_and_add_index_position(df, constants.train_start, constants.train_end, constants.test_start, constants.test_end)
 
     if train_start_idx < 0 or train_end_idx < 0 or test_start_idx < 0 or test_end_idx < 0:
         raise ValueError("One or more index positions are out of bounds.")
 
-    train_data, test_data = split_dataset(scaled_data, train_start_idx, train_end_idx, test_start_idx, test_end_idx)
+    train_data, test_data = utils.split_dataset_into_train_and_test(scaled_data, train_start_idx, train_end_idx, test_start_idx, test_end_idx)
 
     logger.info(f"Train data shape: {train_data.shape}")
     logger.info(f"Test data shape: {test_data.shape}")
@@ -70,8 +35,8 @@ def apply_lstm_forecasting(future_hours):
     sequence_length = constants.lstm_sequence_length
 
     # Create train and test sequences
-    X_train, y_train = create_sequences(train_data, sequence_length)
-    X_test, y_test = create_sequences(test_data, sequence_length)
+    X_train, y_train = utils.create_sequences_to_forecasting(train_data, sequence_length)
+    X_test, y_test = utils.create_sequences_to_forecasting(test_data, sequence_length)
 
     logger.info(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
     logger.info(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
@@ -155,7 +120,7 @@ def apply_lstm_forecasting(future_hours):
     extended_index = last_week_real.index.append(future_dates)
 
     # Plot the results including the future prediction
-    future_text = get_future_text(future_hours)
+    future_text = utils.get_future_text(future_hours)
     plt.figure(figsize=(14, 6))
     plt.plot(extended_index[:len(last_week_real)], last_week_real.values, label='Valor Real')
     plt.plot(extended_index, extended_predictions, label='Valor Previsto e Previsão Futura', linestyle='dashed')
--- src/utility/utils.py
+++ src/utility/utils.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 from utility.config import logger
 
@@ -24,3 +25,35 @@ def remove_columns(df, columns_to_remove):
 def save_csv_file(df, filepath):
     logger.debug(f"Saving file {filepath}")
     df.to_csv(filepath, sep=";", index=False)
+
+def read_csv_file_as_dataframe_with_date_index(file_path, parse_date, index_col):
+    return pd.read_csv(file_path, sep=";", parse_dates=[parse_date], index_col=index_col)
+
+def split_dataset_into_train_and_test(dataset, train_start_idx, train_end_idx, test_start_idx, test_end_idx):
+    train_data = dataset[train_start_idx:train_end_idx]
+    test_data = dataset[test_start_idx:test_end_idx]
+    return train_data, test_data
+
+def create_sequences_to_forecasting(data, seq_length):
+    X, y = [], []
+    for i in range(len(data) - seq_length):
+        X.append(data[i:i + seq_length])
+        y.append(data[i + seq_length])
+    return np.array(X), np.array(y)
+
+def convert_string_to_datetime_and_add_index_position(df, train_start, train_end, test_start, test_end):
+    train_start_idx = df.index.get_loc(pd.to_datetime(train_start), method='nearest')
+    train_end_idx = df.index.get_loc(pd.to_datetime(train_end), method='nearest') + 1
+    test_start_idx = df.index.get_loc(pd.to_datetime(test_start), method='nearest')
+    test_end_idx = df.index.get_loc(pd.to_datetime(test_end), method='nearest') + 1
+
+    return train_start_idx, train_end_idx, test_start_idx, test_end_idx
+
+def get_future_text(future_hours):
+    hour_mapping = {
+        1: "1 hora",
+        12: "12 horas",
+        24: "1 dia",
+        48: "2 dias"
+    }
+    return hour_mapping.get(future_hours, f"{future_hours} horas")
