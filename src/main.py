74094b8a4276fcd0a177c344f93c2e1729835fe6
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2024-06-17 21:50:42 -0300

    fix: reorder imports and methods calling

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import data_processing.process_data as process_data
 import data_processing.data_cleaning as data_cleaning
 import data_processing.data_visualization as data_visualization
-import feature_engineering.feature_prediction as feature_prediction
+import exploratory_data_analysis.data_exploration as data_exploration
 import feature_engineering.feature_selection as feature_selection
 import feature_engineering.pca_analysis as pca_analysis
 import feature_engineering.autoencoder_analysis as autoencoder_analysis
-import exploratory_data_analysis.data_exploration as data_exploration
+import feature_engineering.feature_prediction as feature_prediction
 import forecasting.lstm_time_series as lstm_time_series
 import forecasting.gru_time_series as gru_time_series
 import forecasting.arima_time_series as arima_time_series
@@ -14,11 +14,11 @@ def main():
     # process_data.process_files()
     # data_cleaning.clean_data()
     # data_visualization.apply_measures()
-    # feature_prediction.apply_feature_prediction_classification()
+    # data_exploration.explore_data()
     # feature_selection.apply_feature_selection()
     # pca_analysis.apply_pca_analysis()
     # autoencoder_analysis.apply_autoencoder_analysis()
-    # data_exploration.explore_data()
+    # feature_prediction.apply_feature_prediction_classification()
     # lstm_time_series.apply_lstm_forecasting(1)
     # lstm_time_series.apply_lstm_forecasting(12)
     # lstm_time_series.apply_lstm_forecasting(24)
